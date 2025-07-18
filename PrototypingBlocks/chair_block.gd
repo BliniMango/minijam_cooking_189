f693c92b68ed5016ec49654713272127586c704e
LynnePlex <38146775+BliniMango@users.noreply.github.com>
2025-07-18 10:32:56 -0500

    Protoype 0.0.2
    Updated file names
    Half completed drag and drop mechanic for customers seating

--- PrototypingBlocks/chair_block.gd
+++ PrototypingBlocks/chair_block.gd
@@ -1,8 +1,6 @@
 extends TextureRect
 
 
-
-
 func _get_drag_data(at_position):
 	var preview_texture = TextureRect.new()
 	
@@ -10,7 +8,13 @@ func _get_drag_data(at_position):
 	preview_texture.expand_mode = 1
 	preview_texture.size = Vector2(30,30)
 	
-	return texture
+	var preview = Control.new()
+	preview.add_child(preview_texture)
+	
+	set_drag_preview(preview)
+	
+	texture = null
+	return preview_texture.texture
 
 func _can_drop_data(_pos, data):
 	return data is Texture2D
