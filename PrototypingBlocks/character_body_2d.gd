f15937e5b373a3c4abc5eda0ce74bbe00c26cc83
LynnePlex <38146775+BliniMango@users.noreply.github.com>
2025-07-18 10:03:30 -0500

    Prototype 0.0.1
    Updated chair file name

--- PrototypingBlocks/character_body_2d.gd
+++ PrototypingBlocks/character_body_2d.gd
@@ -11,6 +11,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("left_click"):
 		click_position = get_global_mouse_position()
 		
+		
 	if position.distance_to(click_position) > 3:
 		target_position = (click_position - position).normalized()
 		velocity = target_position * speed
